14266a3688a0220df436d4e967abc93381c5b390
MartinovG <gabismart7@yahoo.com>
2025-07-02 13:31:54 +0300

    db

--- project-name/src/app.module.ts
+++ project-name/src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT ?? '5432', 10),
+      username: process.env.DB_USER || 'postgres',
+      password: process.env.DB_PASSWORD || 'postgres',
+      database: process.env.DB_NAME || 'mydb',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      }),
+  ],
   controllers: [AppController],
   providers: [AppService],
-})
+    })
+
 export class AppModule {}
